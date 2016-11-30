426e4ab6a64655a8979eeef7b0c0e3573c5dcef2
djordjes <djordjesavic@yahoo.com>
2016-11-30 23:41:12 +0100

    added redux, router

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
-import FilterableProductTable from './modules/products/FilterableProductTable';
+import { Router, Route , hashHistory } from 'react-router';
 
-let PRODUCTS = [
-    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-];
+import Product from './modules/products/container';
 
-ReactDOM.render(<FilterableProductTable products={PRODUCTS} />,
-    document.getElementById('app')
-);
+render((
+	<Router history={hashHistory}>
+		<Route path="/" component={Product}/>
+	</Router>
+), document.getElementById('app'));
